6675857501fb02c2658c20407269dd0e4c0ac639
diana890 <diananano@yahoo.com>
2024-09-19 12:12:00 -0400

    creating student list component

--- src/app/student-detail/student-detail.component.ts
+++ src/app/student-detail/student-detail.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+import {User} from "../Shared/Models/user"
+import {NgIf} from "@angular/common";
+
 
 @Component({
   selector: 'app-student-detail',
   standalone: true,
-  imports: [],
+  imports: [NgIf],
   templateUrl: './student-detail.component.html',
   styleUrl: './student-detail.component.css'
 })
 export class StudentDetailComponent {
+
+
+@Input() student?:User;
 
 }
--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,22 +1,33 @@
 import { Component } from '@angular/core';
+import {User} from "../Shared/Models/user";
 import {NgForOf} from "@angular/common";
+import {StudentDetailComponent} from "../student-detail/student-detail.component";
 
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [NgForOf],
+  imports: [NgForOf, StudentDetailComponent],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
 })
 export class StudentListComponent {
 // placeholder for data
-  displayColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
+  displayedColumns: string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
   userList: User[] = [ //Copied from app.component.ts
     {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
-    {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
+    {id: 2, firstName: "Darren", lastName: "Takaki", department: "Web Dev", isAdmin: true},
     {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
   ];
 
+  // catch the click
+  selectedStudent?: User;
+
+//function to display
+  selectStudent(student: User): void {
+    this.selectedStudent = student;
+
+
+  }
 }
