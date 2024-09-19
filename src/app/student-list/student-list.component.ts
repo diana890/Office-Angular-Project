b4947565d1e1cc82a01489248aa8a9a818e27c26
diana890 <diananano@yahoo.com>
2024-09-19 11:43:13 -0400

    creating student list component

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
+import {NgForOf} from "@angular/common";
+
 
 @Component({
   selector: 'app-student-list',
   standalone: true,
-  imports: [],
+  imports: [NgForOf],
   templateUrl: './student-list.component.html',
   styleUrl: './student-list.component.css'
 })
 export class StudentListComponent {
-
+// placeholder for data
+  displayColumns:string[]= ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
   userList: User[] = [ //Copied from app.component.ts
     {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
     {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
