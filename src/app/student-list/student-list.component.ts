c2ed30041d0c571ecd0e55738e7becdf3f46e45c
diana890 <diananano@yahoo.com>
2024-09-19 11:37:18 -0400

    creating student list component

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -9,4 +9,11 @@ import { Component } from '@angular/core';
 })
 export class StudentListComponent {
 
+  userList: User[] = [ //Copied from app.component.ts
+    {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
+    {id: 2, firstName: "Darren", lastName: "Takakki", department: "Web Dev", isAdmin: true},
+    {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
+    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin:true}
+  ];
+
 }
