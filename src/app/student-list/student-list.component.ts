cd42e8e46e720b1a89e448d05fea21699f5ed985
diana890 <diananano@yahoo.com>
2024-09-26 14:38:06 -0400

    create the service

--- src/app/student-list/student-list.component.ts
+++ src/app/student-list/student-list.component.ts
@@ -14,12 +14,7 @@ import {StudentDetailComponent} from "../student-detail/student-detail.component
 export class StudentListComponent {
 // placeholder for data
   displayedColumns: string[] = ['id', 'firstName', 'lastName', 'department', 'isAdmin'];
-  userList: User[] = [ //Copied from app.component.ts
-    {id: 1, firstName: "Matt", lastName: "Haug", department: "Programming", isAdmin: false},
-    {id: 2, firstName: "Darren", lastName: "Takaki", department: "Web Dev", isAdmin: true},
-    {id: 3, firstName: "John", lastName: "Doe", department: "Programming", isAdmin: false},
-    {id: 4, firstName: "Jane", lastName: "Doe", department: "Programming", isAdmin: true}
-  ];
+  userList: User[] = [];
 
   // catch the click
   selectedStudent?: User;
