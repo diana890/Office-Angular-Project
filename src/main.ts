47b3db8627eadc3506f668b8c9d1548d0eb5e4bf
diana890 <diananano@yahoo.com>
2024-10-03 14:49:49 -0400

    add routing

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,20 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import {provideRouter, Routes} from "@angular/router";
+import {StudentListComponent} from "./app/student-list/student-list.component";
+import {StudentDetailComponent} from "./app/student-detail/student-detail.component";
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+
+// add path for two components that we have already
+const routes:Routes=[
+  {path: 'students', component:StudentListComponent},
+  {path: 'students/:id', component: StudentDetailComponent}
+]
+
+
+
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(routes)]
+})
+
