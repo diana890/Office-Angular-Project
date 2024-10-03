660e1e766f4f48c44ddf069801789a5481153fe7
diana890 <diananano@yahoo.com>
2024-10-03 15:03:58 -0400

    add routing

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,16 @@ import { AppComponent } from './app/app.component';
 import {provideRouter, Routes} from "@angular/router";
 import {StudentListComponent} from "./app/student-list/student-list.component";
 import {StudentDetailComponent} from "./app/student-detail/student-detail.component";
+import {ModifyStudentComponent} from "./app/modify-student/modify-student.component";
+import {PageNotFoundComponent} from "./app/page-not-found/page-not-found.component";
 
 
 // add path for two components that we have already
 const routes:Routes=[
   {path: 'students', component:StudentListComponent},
-  {path: 'students/:id', component: StudentDetailComponent}
+  {path: 'students/:id', component: StudentDetailComponent},
+  {path: 'modify-student', component: ModifyStudentComponent},
+  {path: '**', component: PageNotFoundComponent}
 ]
 
 
